d19e2ba110027a3529e778aa73eedaea140133d9
Amit Yadav <kartiktroy00001@gmail.com>
2025-08-10 12:00:53 +0530

    Homepage , Navbar and Resume Card added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import  {resumes}  from "constants/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar></Navbar>
+
+      <section className="main-section">
+        <div className="page-heading py-10">
+          <h1>Track Your Application & Resume Rating</h1>
+          <h2>Review your submission and check AI-powered feedback.</h2>
+        </div>
+       {resumes.length > 0 && (
+         <div className="resumes-section">
+       {resumes.map((resume) => (
+         <ResumeCard key={resume.id} resume={resume}/>
+        ))}
+      </div>
+       )}
+       </section>
+     </main>
 }
